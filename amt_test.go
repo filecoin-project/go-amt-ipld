afaba153c2181b6b620e40d295ac47e2fe9bc79c
whyrusleeping <why@ipfs.io>
2019-09-17 14:25:14 -0700

    Implement 'ForEach' on the AMT

--- amt_test.go
+++ amt_test.go
@@ -1,10 +1,12 @@
 package amt
 
 import (
+	"math/rand"
 	"testing"
 
 	ds "github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-ipfs-blockstore"
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func TestBasicSetGet(t *testing.T) {
@@ -160,5 +162,58 @@ func BenchmarkAMTLoadAndInsert(b *testing.B) {
 		if err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func TestForEach(t *testing.T) {
+	bs := &bstoreWrapper{blockstore.NewBlockstore(ds.NewMapDatastore())}
+	a := NewAMT(bs)
+
+	r := rand.New(rand.NewSource(101))
+
+	var indexes []uint64
+	for i := 0; i < 10000; i++ {
+		if r.Intn(2) == 0 {
+			indexes = append(indexes, uint64(i))
+		}
+	}
+
+	for _, i := range indexes {
+		if err := a.Set(i, "value"); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, i := range indexes {
+		assertGet(t, a, i, "value")
+	}
+
+	assertCount(t, a, uint64(len(indexes)))
+
+	c, err := a.Flush()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	na, err := LoadAMT(bs, c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertCount(t, na, uint64(len(indexes)))
+
+	var x int
+	err = na.ForEach(func(i uint64, v *cbg.Deferred) error {
+		if i != indexes[x] {
+			t.Fatal("got wrong index", i, indexes[x], x)
+		}
+		x++
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if x != len(indexes) {
+		t.Fatal("didnt see enough values")
 	}
 }
