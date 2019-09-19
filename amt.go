7f821452278de1990dbd93e9690e0b9fe7090f1e
whyrusleeping <why@ipfs.io>
2019-09-18 17:25:00 -0700

    add bulk delete method, dumb impl

--- amt.go
+++ amt.go
@@ -211,6 +211,17 @@ func (n *Node) get(bs Blocks, height int, i uint64, out interface{}) error {
 	return subn.get(bs, height-1, i%nodesForHeight(width, height), out)
 }
 
+func (r *Root) BulkDelete(indices []uint64) error {
+	// TODO: theres a faster way of doing this, but this works for now
+	for _, i := range indices {
+		if err := r.Delete(i); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (r *Root) Delete(i uint64) error {
 	if err := r.Node.delete(r.bs, int(r.Height), i); err != nil {
 		return err
