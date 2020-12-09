59f5de3d1e76b3de78a1b6ea4ff6244c5d26e833
Rod Vagg <rod@vagg.org>
2020-12-09 13:55:18 +1100

    chore: add lots of docs

--- amt.go
+++ amt.go
@@ -21,6 +21,8 @@ var log = logging.Logger("amt")
 // don't overflow MaxUint64 when computing the length.
 const MaxIndex = math.MaxUint64 - 1
 
+// Root is described in more detail in its internal serialized form,
+// internal.Root
 type Root struct {
 	bitWidth uint
 	height   int
@@ -31,6 +33,7 @@ type Root struct {
 	store cbor.IpldStore
 }
 
+// NewAMT creates a new, empty AMT root with the given IpldStore and options.
 func NewAMT(bs cbor.IpldStore, opts ...Option) (*Root, error) {
 	cfg := defaultConfig()
 	for _, opt := range opts {
@@ -46,6 +49,10 @@ func NewAMT(bs cbor.IpldStore, opts ...Option) (*Root, error) {
 	}, nil
 }
 
+// LoadAMT loads an existing AMT from the given IpldStore using the given
+// root CID. An error will be returned where the AMT identified by the CID
+// does not exist within the IpldStore. If the given options, or their defaults,
+// do not match the AMT found at the given CID, an error will be returned.
 func LoadAMT(ctx context.Context, bs cbor.IpldStore, c cid.Cid, opts ...Option) (*Root, error) {
 	cfg := defaultConfig()
 	for _, opt := range opts {
@@ -103,6 +110,9 @@ func LoadAMT(ctx context.Context, bs cbor.IpldStore, c cid.Cid, opts ...Option) 
 	}, nil
 }
 
+// FromArray creates a new AMT and performs a BatchSet on it using the vals and
+// options provided. Indexes from the array are used as the indexes for the same
+// values in the AMT.
 func FromArray(ctx context.Context, bs cbor.IpldStore, vals []cbg.CBORMarshaler, opts ...Option) (cid.Cid, error) {
 	r, err := NewAMT(bs)
 	if err != nil {
@@ -115,11 +125,30 @@ func FromArray(ctx context.Context, bs cbor.IpldStore, vals []cbg.CBORMarshaler,
 	return r.Flush(ctx)
 }
 
+// Set will add or update entry at index i with value val. The index must be
+// within lower than MaxIndex.
+//
+// Where val has a compatible CBORMarshaler() it will be used to serialize the
+// object into CBOR. Otherwise the generic go-ipld-cbor DumbObject() will be
+// used.
+//
+// Setting a new index that is greater than the current capacity of the
+// existing AMT structure will result in the creation of additional nodes to
+// form a structure of enough height to contain the new index.
+//
+// The height required to store any given index can be calculated by finding
+// the lowest (width^(height+1) - 1) that is higher than the index. For example,
+// a height of 1 on an AMT with a width of 8 (bitWidth of 3) can fit up to
+// indexes of 8^2 - 1, or 63. At height 2, indexes up to 511 can be stored. So a
+// Set operation for an index between 64 and 511 will require that the AMT have
+// a height of at least 3. Where an AMT has a height less than 3, additional
+// nodes will be added until the height is 3.
 func (r *Root) Set(ctx context.Context, i uint64, val interface{}) error {
 	if i > MaxIndex {
 		return fmt.Errorf("index %d is out of range for the amt", i)
 	}
 
+	// serialize the value however we can
 	var b []byte
 	if m, ok := val.(cbg.CBORMarshaler); ok {
 		buf := new(bytes.Buffer)
@@ -135,15 +164,24 @@ func (r *Root) Set(ctx context.Context, i uint64, val interface{}) error {
 		}
 	}
 
+	// where the index is greater than the number of elements we can fit into the
+	// current AMT, grow it until it will fit.
 	for i >= nodesForHeight(r.bitWidth, r.height+1) {
+		// if we have existing data, perform the re-height here by pushing down
+		// the existing tree into the left-most portion of a new root
 		if !r.node.empty() {
 			nd := r.node
+			// since all our current elements fit in the old height, we _know_ that
+			// they will all sit under element [0] of this new node.
 			r.node = &node{links: make([]*link, 1<<r.bitWidth)}
 			r.node.links[0] = &link{
 				dirty:  true,
 				cached: nd,
 			}
 		}
+		// else we still need to add new nodes to form the right height, but we can
+		// defer that to our set() call below which will lazily create new nodes
+		// where it expects there to be some
 		r.height++
 	}
 
@@ -157,12 +195,22 @@ func (r *Root) Set(ctx context.Context, i uint64, val interface{}) error {
 		if r.count >= (MaxIndex - 1) {
 			return errInvalidCount
 		}
+		// Count is only stored in the root and not validated as being correct,
+		// just incremented and decremented in _this implementation_ but may not
+		// necessarily reflect the state of the graph if generated from untrusted
+		// sources.
 		r.count++
 	}
 
 	return nil
 }
 
+// BatchSet takes an array of vals and performs a Set on each of them on an
+// existing AMT. Indexes from the array are used as indexes for the same values
+// in the AMT.
+//
+// This is currently a convenience method and does not perform optimizations
+// above iterative Set calls for each entry.
 func (r *Root) BatchSet(ctx context.Context, vals []cbg.CBORMarshaler) error {
 	// TODO: there are more optimized ways of doing this method
 	for i, v := range vals {
@@ -173,11 +221,16 @@ func (r *Root) BatchSet(ctx context.Context, vals []cbg.CBORMarshaler) error {
 	return nil
 }
 
+// Get retrieves a value from index i and deserializes the value into the out
+// interface. Where a specified index does not exist in the AMT, an ErrNotFound
+// error is returned.
 func (r *Root) Get(ctx context.Context, i uint64, out interface{}) error {
 	if i > MaxIndex {
 		return fmt.Errorf("index %d is out of range for the amt", i)
 	}
 
+	// easy shortcut case, index is too large for our height, don't bother looking
+	// further
 	if i >= nodesForHeight(r.bitWidth, int(r.height+1)) {
 		return &ErrNotFound{Index: i}
 	}
@@ -189,6 +242,14 @@ func (r *Root) Get(ctx context.Context, i uint64, out interface{}) error {
 	return nil
 }
 
+// BatchDelete performs a bulk Delete operation on an array of indices. Each
+// index in the given indices array will be removed from the AMT. Where an
+// index does not exist in the AMT, the batch operation will be halted and an
+// ErrNotFound error returned. This operation is not atomic and an ErrNotFound
+// may result in only some of the indices removed from the AMT.
+//
+// There is no special optimization applied to this method, it is simply a
+// convenience wrapper around Delete for an array of indices.
 func (r *Root) BatchDelete(ctx context.Context, indices []uint64) error {
 	// TODO: theres a faster way of doing this, but this works for now
 
@@ -209,10 +270,19 @@ func (r *Root) BatchDelete(ctx context.Context, indices []uint64) error {
 	return nil
 }
 
+// Delete removes an index from the AMT. If the index isn't present in the AMT
+// (either because the AMT is larger than index or it's not set) an ErrNotFound
+// error is returned.
+//
+// Compaction will be performed if this delete leaves nodes with no remaining
+// elements leaving the AMT in canonical form for the given set of data that
+// it contains.
 func (r *Root) Delete(ctx context.Context, i uint64) error {
 	if i > MaxIndex {
 		return fmt.Errorf("index %d is out of range for the amt", i)
 	}
+
+	// shortcut, index is greater than what we hold so we know it's not there
 	if i >= nodesForHeight(r.bitWidth, int(r.height+1)) {
 		return &ErrNotFound{i}
 	}
@@ -224,6 +294,17 @@ func (r *Root) Delete(ctx context.Context, i uint64) error {
 		return &ErrNotFound{i}
 	}
 
+	// The AMT invariant dictates that for any non-empty AMT, the root node must
+	// not address only its left-most child node. Where a deletion has created a
+	// state where the current root node only consists of a link to the left-most
+	// child and no others, that child node must become the new root node (i.e.
+	// the height is reduced by 1). We perform the same check on the new root node
+	// such that we reduce the AMT to canonical form for this data set.
+	// In the extreme case, it is possible to perform a collapse from a large
+	// `height` to height=0 where the index being removed is very large and there
+	// remains no other indexes or the remaining indexes are in the range of 0 to
+	// bitWidth^8.
+	// See node.collapse() for more notes.
 	newHeight, err := r.node.collapse(ctx, r.store, r.bitWidth, r.height)
 	if err != nil {
 		return err
@@ -236,11 +317,15 @@ func (r *Root) Delete(ctx context.Context, i uint64) error {
 		return errInvalidCount
 	}
 
+	// decrement count - advisory only in the root node!
 	r.count--
 	return nil
 }
 
-// Subtract removes all elements of 'or' from 'r'
+// Subtract removes all elements of 'or' from this AMT. Uses a ForEach
+// operation on the passed AMT to perform a deletion on this AMT. This method
+// is a convenience and does not perform any internal optimizations for a
+// batch.
 func (r *Root) Subtract(ctx context.Context, or *Root) error {
 	// TODO: as with other methods, there should be an optimized way of doing this
 	return or.ForEach(ctx, func(i uint64, _ *cbg.Deferred) error {
@@ -248,18 +333,28 @@ func (r *Root) Subtract(ctx context.Context, or *Root) error {
 	})
 }
 
+// ForEach iterates over the entire AMT and calls the cb function for each
+// entry found in the leaf nodes. The callback will receive the index and the
+// value of each element.
 func (r *Root) ForEach(ctx context.Context, cb func(uint64, *cbg.Deferred) error) error {
 	return r.node.forEachAt(ctx, r.store, r.bitWidth, r.height, 0, 0, cb)
 }
 
+// ForEachAt iterates over the AMT beginning from the given start index. See
+// ForEach for more details.
 func (r *Root) ForEachAt(ctx context.Context, start uint64, cb func(uint64, *cbg.Deferred) error) error {
 	return r.node.forEachAt(ctx, r.store, r.bitWidth, r.height, start, 0, cb)
 }
 
+// FirstSetIndex finds the lowest index in this AMT that has a value set for
+// it. If this operation is called on an empty AMT, an ErrNoValues will be
+// returned.
 func (r *Root) FirstSetIndex(ctx context.Context) (uint64, error) {
 	return r.node.firstSetIndex(ctx, r.store, r.bitWidth, r.height)
 }
 
+// Flush saves any unsaved node data and recompacts the in-memory forms of each
+// node where they have been expanded for operational use.
 func (r *Root) Flush(ctx context.Context) (cid.Cid, error) {
 	nd, err := r.node.flush(ctx, r.store, r.bitWidth, r.height)
 	if err != nil {
@@ -274,10 +369,16 @@ func (r *Root) Flush(ctx context.Context) (cid.Cid, error) {
 	return r.store.Put(ctx, &root)
 }
 
+// Len returns the advisory "count" of this AMT. This is not strictly a secure
+// count of the number of elements, but is assumed correct unless the AMT
+// contains nodes produced by untrusted actors. A secure count would require the
+// full iteration of leaf nodes to count values.
 func (r *Root) Len() uint64 {
 	return r.count
 }
 
+// ErrNotFound is used to indicate the non-existence of an entry on retrieval
+// operations.
 type ErrNotFound struct {
 	Index uint64
 }
@@ -286,6 +387,7 @@ func (e ErrNotFound) Error() string {
 	return fmt.Sprintf("Index %d not found in AMT", e.Index)
 }
 
+// NotFound can be used for type checking this error type.
 func (e ErrNotFound) NotFound() bool {
 	return true
 }
